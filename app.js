0331cd81df69ccfbeb27e747f90823adc451f5ea
Surkhab Singh <surkhab26singh@gmail.com>
2024-04-17 12:16:39 +0530

    Initialized Project: added some new features

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const express = require("express");
 
 const expressLayouts = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
+const post = require("./server/models/post");
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+connectDB();
 
 app.use(express.static("public"));
 
@@ -13,8 +19,32 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
+
 app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => {
   console.log(`APP listening on PORT ${PORT}`);
 });
+
+// // function insertPostData() {
+// //   post.insertMany([
+// //     {
+// //       title: "dummyData",
+// //       body: "Body of dummy data",
+// //     },
+// //   ]);
+// // }
+
+// function generateDummyPosts() {
+//   return {
+//     title: `Post ${Math.floor(Math.random() * 1000)}`,
+//     body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et sem eu lorem eleifend dapibus. Ut vitae ultricies leo`,
+//   };
+// }
+// const dummyPosts = Array.from({ length: 20 }, generateDummyPosts);
+// // console.log(dummyPosts);
+
+// function insertPostData() {
+//   post.insertMany(dummyPosts);
+// }
+// insertPostData();
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const post = require("../models/post");
 
-router.get("", (req, res) => {
-  const locals = {
-    title: "NodeJs",
-    description: "NodeJs,Mongodb and Express ",
-  };
-  res.render("index", { locals });
+router.get("", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs",
+      description: "NodeJs,Mongodb and Express ",
+    };
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await post
+      .aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
@@ -18,3 +42,16 @@ router.get("/contact", (req, res) => {
 });
 
 module.exports = router;
+
+// router.get("", async (req, res) => {
+//   const locals = {
+//     title: "NodeJs",
+//     description: "NodeJs,Mongodb and Express ",
+//   };
+//   try {
+//     const data = await post.find();
+//     res.render("index", { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
